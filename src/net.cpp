d30f8a225bfc7c8230e4bd4318c3f6759f83fea2
LeoS9 <45266756+LeoS9@users.noreply.github.com>
2019-08-03 16:00:10 +0200

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1132,7 +1132,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-		    	{"DNS-SEED", "185.82.202.157"},	
+		    	{"DNS-SEED", "185.82.202.154"},	
 };
 
 void ThreadDNSAddressSeed(void* parg)
